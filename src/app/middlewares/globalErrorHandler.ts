a9fb8b9133f85309e369a393528234fcc9121814
faishal-developer <foysal.professional@gmail.com>
2023-09-14 13:22:34 +0600

    kdk

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -68,7 +68,7 @@ const globalErrorHandler: ErrorRequestHandler = (
     success: false,
     message,
     errorMessages,
-    stack: config.env !== 'production' ? error?.stack : undefined,
+    stack: error?.stack,
   });
 };
 
